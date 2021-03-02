cd4446b49c48afcb6ed204b477eaf70cf1c6b93b
Alberto Parra <a.parra.asenjo@gmail.com>
2021-03-01 21:48:11 -0300

    desafio blog RoR

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  http_basic_authenticate_with name: "g37", password: "password", only:
+:dashboard
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  def create
+    @post = Post.create(title: params[:title],
+                        image_url: params[:image_url],
+                        content: params[:content])
+  end
+  def dashboard
+    @posts = Post.all
+  end
+end
